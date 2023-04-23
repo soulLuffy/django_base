3b1a66e7b3bc83824b260c5fc1e73b269072e04b
itheima <itheima@py.com>
2023-04-23 20:03:19 +0800

    创建数据表

--- bookmanager/book/models.py
+++ bookmanager/book/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class BookInfo(models.Model):
+    """图书信息表"""
+    name = models.CharField(max_length=20)
+
+
+class PeopleInfo(models.Model):
+    """人物信息表"""
+    name = models.CharField(max_length=20)
+    gender = models.BooleanField()
+    book = models.ForeignKey(BookInfo, on_delete=models.CASCADE)
