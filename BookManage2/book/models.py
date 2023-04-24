5448270213a44f17ce6d01cf5ec1a5c75fcf676a
itheima <itheima@py.com>
2023-04-24 11:08:15 +0800

    创建数据表

--- BookManage2/book/models.py
+++ BookManage2/book/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+class BookInfo(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+    pub_date = models.DateField(null=True)
+    read_count = models.IntegerField(default=0)
+    comment_count = models.IntegerField(default=0)
+    is_delete = models.BooleanField(default=False)  # 布尔值
+
+    class Meta:
+        db_table = 'BookInfo'  # 自定义数据表名称
+        verbose_name = '书籍管理'  # admin站点使用
